316732e9217e71e665e9d417845210be23cf153b
gatno <danieltaucher@gmail.com>
2019-01-19 01:13:18 +0100

    Fixed return type of registerManager

--- src/SofortServiceProvider.php
+++ src/SofortServiceProvider.php
@@ -45,7 +45,7 @@ class SofortServiceProvider extends ServiceProvider
     /**
      * Register the factory class.
      */
-    protected function registerFactory(): string
+    protected function registerFactory()
     {
         $this->app->singleton('sofort.factory', function () {
             return new SofortFactory();
@@ -57,7 +57,7 @@ class SofortServiceProvider extends ServiceProvider
     /**
      * Register the manager class.
      */
-    protected function registerManager(): string
+    protected function registerManager()
     {
         $this->app->singleton('sofort', function (Container $app) {
             $config = $app['config'];
